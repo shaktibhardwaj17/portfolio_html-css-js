93ddbd6d61b72419264a769a7fc9de7105695b38
Shakti_Bhardwaj <shaktibhardwaj2030@gmail.com>
2024-05-17 00:03:37 +0530

    messaging changed

--- script.js
+++ script.js
@@ -88,3 +88,23 @@ $(document).ready(function () {
       }
     });
   });
+
+
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbz7qgMD5c-mJk8mX6ZmTh3lLPMp-9A5yhMSnuaFya8dWp0_mAklERy5wtfBzId8Zj97/exec';
+  const form = document.forms['submit-to-google-sheet']
+  
+  const msg = document.getElementById("msg")
+
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML = "Message sent successfully"
+        setTimeout(function(){
+          msg.innerHTML = ""
+        },5000)
+        form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
+  });
